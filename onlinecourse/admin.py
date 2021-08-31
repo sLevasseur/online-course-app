65a738474b93c1f8e76a837195e6bfa158bf9750
sLevasseur <simonphilipp.levasseur@gmail.com>
2021-08-31 08:40:20 +0000

    add  view for questions and choices

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 10
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extrra = 4
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -12,7 +18,7 @@ class LessonInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -21,10 +27,19 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['course_related', 'question_content', 'grade_point']
+    search_fields = ['question_content', 'course_related']
+    list_filter = ['grade_point']
 
-# <HINT> Register Question and Choice models here
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_content', 'question_related']
+    list_filter = ['is_correct']
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
